c70f3c5c76c2cf5b302799d4329ae7462ad25bde
tomomikinoshita115 <loving_you_one_love@yahoo.co.jp>
2023-06-10 19:02:54 +0900

    App

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="title">TODO</h1>
+</template>
+<style scoped>
+.title {
+  width: 100%;
+  background-color: #e3f2fd;
+  text-align: center;
+  font-size: 32px;
+  font-weight: bold;
+  padding: 8px 0;
+}
+</style>
